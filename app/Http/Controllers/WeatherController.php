146735e3853b0172993595dfa5f5a35617048ce6
Ray Tsou <ray.tsou@ilong-termcare.com>
2019-10-06 03:03:02 +0800

    Task : move const parameter to out layer.

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,20 +7,31 @@ use GuzzleHttp\Client;
 
 class WeatherController extends Controller
 {
-	public function index()
-	{
-		$api_key = 'c17055de8dfd5c5f23ca798461ee906d';
-		$weather_api = 'http://api.openweathermap.org/data/2.5/';
-		$get_current_method = 'weather';
-		$client = new \GuzzleHttp\Client();
-		$citys = ['tokyo'=>'Jezael Melgoza', 'yokohama'=>'Jezael Melgoza', 'kyoto'=>'Sorasak', 'osaka'=>'Koi Visuals', 'sapporo'=>'Jaeyoung Geoffrey', 'nagoya'=>'Teo Zac'];
-		$weathers = [];
-		foreach ($citys as $city => $author)
-		{
-			$response = $client->request('GET', $weather_api.$get_current_method.'?q='.$city.'&APPID='.$api_key);
-			$weathers[$city] = json_decode($response->getBody(), true);
-			$weathers[$city]['author'] = $author;
-		}
-		return view("weather")->with("weathers", $weathers);
-	}
+    private $weather_api_key = 'c17055de8dfd5c5f23ca798461ee906d';
+    private $weather_api_url = 'http://api.openweathermap.org/data/2.5/';
+    private $citys = ['tokyo'=>'Jezael Melgoza', 
+                      'yokohama'=>'Jezael Melgoza',
+                      'kyoto'=>'Sorasak',
+                      'osaka'=>'Koi Visuals',
+                      'sapporo'=>'Jaeyoung Geoffrey',
+                      'nagoya'=>'Teo Zac'];
+
+    public function index()
+    {
+        $weathers = [];
+        foreach ($this->citys as $city => $author)
+        {
+            $weathers[$city] = $this->getCurrentWeatherByCityName($city, $author);
+            $weathers[$city]['author'] = $author;
+        }
+        return view("weather")->with("weathers", $weathers);
+    }
+
+    private function getCurrentWeatherByCityName($city, $author)
+    {
+        $client = new \GuzzleHttp\Client();
+        $api_method = 'weather';
+        $response = $client->request('GET', $this->weather_api_url.$api_method.'?q='.$city.'&APPID='.$this->weather_api_key);
+        return json_decode($response->getBody(), true);
+    }
 }
